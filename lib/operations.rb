e466068dcda4b0f550bbd6da488d21f2fc8af034
Adam Palmer <theadampalmer@gmail.com>
2020-07-27 20:06:51 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,24 +1,16 @@
 
 
-# def unsafe?(speed)
- 
-#   speed > 60 || speed < 40
-  
-# end
-
-
-
-# def not_safe?(speed)
-  
-#   speed < 40 || speed > 60 ? true : false
-	
-# end
-	
 def unsafe?(speed)
+ 
   speed > 60 || speed < 40
+  
 end
+
+
 
 def not_safe?(speed)
-  ? speed > 60 : false
-  ? speed < 40 : false
+  
+  speed < 40 || speed > 60 ? true : false
+	
 end
+
